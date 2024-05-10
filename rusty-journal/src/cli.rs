2cd5c3d802f5c2506aaacf6d8915a10123ad0664
Mohamed Hazem <mohamedhazemwwe@gmail.com>
2024-05-10 22:16:39 +0300

    tasks.rs Done

---
+++ rusty-journal/src/cli.rs
@@ -0,0 +1,35 @@
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+
+#[derive(Debug, StructOpt)]
+pub enum Action{
+    /// Write tasks to journal
+    Add {
+        /// The task description text
+        #[structopt()]
+        task : String
+    }, 
+    /// Remove entry from journal file by position
+    Done {
+        #[structopt()]
+        position : usize
+    },
+    /// List allt tasks in the journal
+    List
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "Rusty Journal",
+    about = "A command-line to-do app written in rust"
+)]
+pub struct CommandLineArgs {
+    #[structopt(subcommand)]
+    pub action : Action,
+
+    /// Use a different journal file
+    #[structopt(parse(from_os_str), short, long)]
+    pub journal_file : Option<PathBuf>,
+}
+
